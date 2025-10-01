75d7c54cc8ed66c57038405752580d267b61119c
Mahsa Esfandiaripour <11901872@student.pxl.be>
2025-10-01 14:38:25 +0200

    Eind

--- Popups/MainPage.xaml.cs
+++ Popups/MainPage.xaml.cs
@@ -8,11 +8,55 @@ namespace Popups
             InitializeComponent();
         }
 
+
+
         private Random rnd = new Random(); 
         private Color GetRandomColor()
         {
             return Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
         }
+
+        private async void OnAdd_Clicked(object sender, EventArgs e)
+        {
+            string name = await DisplayPromptAsync("Toevoegen", "Naam: ");
+
+            if (!string.IsNullOrWhiteSpace(name)) {
+
+                Button button = new Button();
+                button.Text = name;
+                button.BackgroundColor = GetRandomColor();
+                button.Clicked += OnName_Clicked;
+
+                flex.Children.Add(button);
+
+            }
+
+           
+        }
+
+        private async void OnName_Clicked(object? sender, EventArgs e)
+        {
+            // Button button = sender as Button;
+            // if (button != null) { }
+
+            if (sender is Button button)
+            {
+                //button.BackgroundColor = GetRandomColor();
+
+                string name = button.Text;
+                bool answer = await DisplayAlert("Verwijderen?", $"Bent u zeker dat u {name} wil verwijderen?", "Yes", "No");
+                if (answer ==true)
+                {
+                    flex.Children.Remove(button);
+                }
+
+            }
+             
+            
+
+        }
+
+        
     }
 
 }
